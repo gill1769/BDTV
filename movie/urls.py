3fdbb66fd75b7636017d06c5ce1425929ba360f3
Harjinder Gill <harjindergill1769@gmail.com>
2022-04-03 22:06:35 -0400

    Search

--- movie/urls.py
+++ movie/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('new/', views.new, name='new'),
-    path('search/',views.search,name='search')
+    path('search/',views.search,name='search'),
+    path('movie_details/<movie_id>/',views.movie_detail,name='movie_details')
 ]
--- movie/views.py
+++ movie/views.py
@@ -27,13 +27,28 @@ def new(request):
 
 def search(request):
     form = SearchForm()
-    re=''
+    data=''
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data.get('title')
-            movie = Movie()
-            re = movie.getMovie(title)
+            url='https://api.themoviedb.org/3/search/movie?api_key=e4b3e22ec2b42c496bb2127725b996ad&language=en-US&query='+title
+
+            response = requests.request("GET", url)
+            data = json.loads(response.text)
 
     # return redirect(home)
-    return render(request, 'movie/search_result.html', {'form': form,'re':re})
+    return render(request, 'movie/search_result.html', {'form': form,'data':data})
+
+def movie_detail(request,movie_id):
+    url = 'https://api.themoviedb.org/3/movie/'+str(movie_id)+'?api_key=e4b3e22ec2b42c496bb2127725b996ad&language=en-US'
+
+    response = requests.request("GET", url)
+    data = json.loads(response.text)
+    context = {
+        'title':data['original_title'],
+        'des':data['overview'],
+        'rating': data['vote_average'],
+        'release_date': data['release_date']
+    }
+    return render(request,'movie/movie_details.html',{'context':context})
